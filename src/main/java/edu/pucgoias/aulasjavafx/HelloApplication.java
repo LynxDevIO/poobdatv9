17701768521cb6b16f0f2202759cea0277b1154f
Thalles Santos <thallesbgns@gmail.com>
2025-03-18 19:03:17 -0300

    Refatoração do projeto JavaFX.

--- src/main/java/edu/pucgoias/aulasjavafx/HelloApplication.java
+++ src/main/java/edu/pucgoias/aulasjavafx/HelloApplication.java
@@ -3,54 +3,26 @@ package edu.pucgoias.aulasjavafx;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class HelloApplication extends Application
-        implements EventHandler<ActionEvent> {
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/edu/pucgoias/aulasjavafx/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 200);
+        stage.setTitle("Tratando eventos - MVC");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
         launch();
-    }
-
-    @Override
-    public void start(Stage palco) throws Exception {
-        VBox raiz = new VBox(20);
-        raiz.setAlignment(Pos.CENTER);
-        raiz.setTranslateY(5);
-
-        Button btnUm = new Button("Clique em mim! (Tratador externo)");
-        Button btnDois = new Button("Clique em mim! (Expressão Lambda)");
-        Button btnTres = new Button("Clique em mim! (Própria classe)");
-
-        // usamos a classe TratadorEvento para cuidar dos eventos
-        btnUm.setOnAction(new TratadorEvento());
-        // Criando uma instância de uma classe anônima para tratar evento
-        btnDois.setOnAction(new EventHandler<ActionEvent>() {
-
-            public void handle(ActionEvent evento) {
-                System.out.println("Evento tratado por uma classe anônima!");
-            }
-        });
-        // o botão 3 usa essa própria classe para tratar seus eventos
-        btnTres.setOnAction(this);
-
-        raiz.getChildren().addAll(btnUm, btnDois, btnTres);
-
-        Scene cena = new Scene(raiz, 300, 200);
-        palco.setTitle("Tratando eventos");
-        palco.setScene(cena);
-        palco.show();
-    }
-
-    /*
-     * Como a própria classe TratamentoEventoComBotao implementa
-     * a interface EventHandler, ela mesma pode responder a eventos do botão
-     *
-     */
-    public void handle(ActionEvent evento) {
-        System.out.println("Evento tratado na próxima classe!");
     }
 }
--- src/main/java/edu/pucgoias/aulasjavafx/HelloController.java
+++ src/main/java/edu/pucgoias/aulasjavafx/HelloController.java
@@ -1,14 +1,32 @@
 package edu.pucgoias.aulasjavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btnUm;
+
+    @FXML
+    private Button btnDois;
+
+    @FXML
+    private Button btnTres;
+
+    @FXML
+    private void botaoUmClicado(ActionEvent evento) {
+        System.out.println("Evento tratado por um controlador externo!");
+    }
+
+    @FXML
+    private void botaoDoisClicado(ActionEvent evento) {
+        System.out.println("Evento tratado via expressão Lambda!");
+    }
+
+    @FXML
+    private void botaoTresClicado(ActionEvent evento) {
+        System.out.println("Evento tratado na própria classe controladora!");
     }
 }
