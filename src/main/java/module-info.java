13b5f9854c87a30848eb14f4789a3c064d43ea94
philipe <dev.lynx.io@gmail.com>
2025-03-20 15:13:29 -0300

    melhoria no código

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,14 +2,15 @@ module edu.pucgoias.aulasjavafx {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-
     opens edu.pucgoias.aulasjavafx to javafx.fxml;
     exports edu.pucgoias.aulasjavafx;
-    exports edu.pucgoias.aulasjavafx.view;
+
     opens edu.pucgoias.aulasjavafx.view to javafx.fxml;
-    exports edu.pucgoias.aulasjavafx.controller;
+    exports edu.pucgoias.aulasjavafx.view;
+
     opens edu.pucgoias.aulasjavafx.controller to javafx.fxml;
-    exports edu.pucgoias.aulasjavafx.util;
+    exports edu.pucgoias.aulasjavafx.controller;
+
     opens edu.pucgoias.aulasjavafx.util to javafx.fxml;
+    exports edu.pucgoias.aulasjavafx.util;
 }
